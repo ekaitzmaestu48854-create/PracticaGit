0248547aa3b31f28c1089078fa557f1b82381865
ekai2 <ekaitz.maestu.48854@ikasle.egibide.org>
2025-10-22 12:06:06 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,45 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cad = "";
+        int n1;
+        do {
 
-        }
+            System.out.println("------MENU------");
+            System.out.println("1.Solicitar cadena:");
+            System.out.println("2.Visualizar-posicion");
+            System.out.println("3.Convertir-numero-entero");
+            System.out.println("4.Convertir-fecha(dd/MM/yyyy)");
+            System.out.println("5.Salir");
+            System.out.print("Selecciona 1 opcion para continuar: ");
+            n1 = sc.nextInt();
+            switch (n1) {
+                case 1:
+                    cad = solicitarcadena(cad);
+                    break;
+                case 2:
+                    Visualizar_posicion(cad);
+                    break;
+                case 3:
+                    Comvertir_numentero(cad);
+                    break;
+                case 4:
+                    Convertir_fecha(cad);
+                    break;
+                case 5:
+                    System.out.println("Saliendo...");
+                    System.exit(0);
+                default:
+                    System.out.println("valor no reconocido");
+                    break;
+            }
+        }while (n1 != 5);
     }
+
+
 }
