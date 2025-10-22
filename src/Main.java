8f3bde0a0d49ae3ceed7f00894ad6499d5519ed1
ekai2 <ekaitz.maestu.48854@ikasle.egibide.org>
2025-10-22 12:20:14 +0200

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -38,13 +38,46 @@ public class Main {
                     System.out.println("valor no reconocido");
                     break;
             }
-        }while (n1 != 5);
+        } while (n1 != 5);
     }
-    public static String solicitarcadena(String cad){
+
+    public static String solicitarcadena(String cad) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce una cadena de caracteres:");
         cad = sc.nextLine();
         return cad;
     }
 
+    public static void Visualizar_posicion(String cad) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Que posicion quieres visualizar?");
+            int posicion = sc.nextInt();
+            System.out.println(cad.charAt(posicion));
+            throw new NumberFormatException("Error");
+        } catch (NumberFormatException e) {
+            System.out.println("La posicion es un numero" + e.getClass());
+        }
+    }
+
+    public static void Comvertir_numentero(String cad) {
+        try {
+            double cadena = Double.parseDouble(cad);
+            System.out.println("Convertido a numero entero: " + cadena);
+        } catch (Exception e) {
+            System.out.println("Error en la convertida/No es un valor permitido" + e.getMessage());
+        }
+
+    }
+
+    public static void Convertir_fecha(String cad) {
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDateTime fecha = LocalDateTime.parse(cad, dtf);
+            System.out.println(fecha);
+        } catch (Exception e) {
+            System.out.println("Error en la convertida/No es un valor permitido" + e.getClass());
+        }
+
+    }
 }
